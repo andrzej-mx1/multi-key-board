5b0d576cb68b31f87b5c17b8457ed3ced8dca8eb
andrzej-mx1 <91850478+andrzej-mx1@users.noreply.github.com>
2021-10-03 22:06:40 +0200

    Add files via upload

---
+++ analog_keyboard_resistiors_calculator.py
@@ -0,0 +1,27 @@
+import argparse
+import sys
+
+sys.argv = ["analog_keyboard_resistor_calcultor.py", '--Resistor=1500', '--n=11']
+int_resistor=0
+int_n=0
+int_resistors_values = []
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--Resistor', type=int, choices=range(1,10000), default=1500, help='Resistor R value in Ohms')
+parser.add_argument('--n', type=int, choices=range(2,101), default=3, help='Number of the keys.')
+args = parser.parse_args()
+#print(args)
+
+int_resistor=args.Resistor
+int_n = args.n
+
+for index in range(1,int_n+1):
+    int_resistors_values.append(int(((int_n+1)*int_resistor)/((int_n-index+2)*(int_n-index+1))+0.5))
+
+#print(int_resistors_values)
+print(int_resistor)
+for str_line in int_resistors_values:
+    print(str_line)
+    
+
+
---
+++ analog_keyboard_values.py
@@ -0,0 +1,90 @@
+import argparse
+import sys
+
+int_ADC_readings = []
+int_ADC_boundaries = []
+int_ADC_ranges = []
+int_ADC_max_reading = 1024
+int_resistors_values = []
+float_voltage_values = []
+float_voltage_divider_output = []
+
+#
+# Configure args parser
+#
+parser = argparse.ArgumentParser()
+parser.add_argument('--file', type=str, default='resistors.txt', help='Filename wiht resistors values.')
+parser.add_argument('--VCC', type=float, choices=range(0,255), default=5, help='Value of Vcc.')
+args = parser.parse_args()
+
+float_Vcc=args.VCC
+
+#
+# Open file and read resistors values from it. 
+#
+with open(args.file) as f:
+    for read_line in f:
+        i=int(read_line)
+        int_resistors_values.append(i)
+
+#
+# Calculate voltage values. 
+#
+float_nominator = 0;
+float_denominator = 0;
+for i in range(0, len(int_resistors_values)):
+        float_denominator += int_resistors_values[i]
+        float_voltage_values.append(float((float_nominator/float_denominator)*float_Vcc))
+        if i<(len(int_resistors_values)-1):
+            float_nominator += int_resistors_values[i+1]
+#
+# Calculates ADC readings 
+#
+for float_voltage in float_voltage_values:
+  x =  int(int_ADC_max_reading*float_voltage/5+0.5)
+  int_ADC_readings.append(x)
+
+
+#
+# Calculate ADC readings ranges boundary 
+#
+int_ADC_boundaries.append(0);
+for index in range(0,len(int_ADC_readings)-1): 
+  x =int((int_ADC_readings[index]+int_ADC_readings[index+1])/2+0.5)
+  int_ADC_boundaries.append(x)
+
+int_ADC_boundaries.append(int_ADC_max_reading);
+
+
+#
+# Build table of ranges
+#
+for index in range(0,len(int_ADC_boundaries)-1):
+  int_ADC_one_range = []
+  l = int_ADC_boundaries[index]
+  h = int_ADC_boundaries[index+1]
+  int_ADC_one_range.append(l)
+  int_ADC_one_range.append(h)
+  int_ADC_ranges.append(int_ADC_one_range)
+  
+
+
+#
+# Prints table with ADC eading ranges, in C/C++ syntax.
+#
+i=0
+str_output = "{"
+for range in int_ADC_ranges:
+    str_output += "{"
+    str_output += str(range[0])
+    str_output += ","
+    str_output += str(range[1])
+    str_output += "}"
+    i=i+1
+    if i <len(int_ADC_ranges):
+        str_output += ","
+str_output += "}"
+
+print(str_output)
+
+
