4416bc010e0b9a67fd488c53129cabf3ad72b393
andrzej-mx1 <sp9amo@gmail.com>
2021-10-06 22:42:12 +0200

    Update analog_keyboard_resistiors_calculator.py

--- analog_keyboard_resistiors_calculator.py
+++ analog_keyboard_resistiors_calculator.py
@@ -6,6 +6,9 @@ int_resistor=0
 int_n=0
 int_resistors_values = []
 
+#
+# Configure Args Parser
+#
 parser = argparse.ArgumentParser()
 parser.add_argument('--Resistor', type=int, choices=range(1,10000), default=1500, help='Resistor R value in Ohms')
 parser.add_argument('--n', type=int, choices=range(2,101), default=3, help='Number of the keys.')
@@ -15,10 +18,15 @@ args = parser.parse_args()
 int_resistor=args.Resistor
 int_n = args.n
 
+#
+# Calculates resistors values
+#
 for index in range(1,int_n+1):
     int_resistors_values.append(int(((int_n+1)*int_resistor)/((int_n-index+2)*(int_n-index+1))+0.5))
 
-#print(int_resistors_values)
+#
+# Prints calcualted values to the stdout
+#
 print(int_resistor)
 for str_line in int_resistors_values:
     print(str_line)
